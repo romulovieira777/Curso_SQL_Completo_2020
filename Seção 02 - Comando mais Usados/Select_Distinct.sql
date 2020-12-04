de6e8ab403709381e625002194e5fc2ff6553fbf
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-04 19:27:34 -0300

    Select_Distinct

---
+++ Seção 02 - Comando mais Usados/Select_Distinct.sql
@@ -0,0 +1,19 @@
+/* Escolhendo um Banco de Dados */
+USE
+	[AdventureWorks2017]
+GO
+
+
+/* Selecionando Dados da Tabelas Únicos */
+SELECT DISTINCT
+	FirstName
+FROM
+	Person.Person
+GO
+
+
+/* Questão Desafio */
+SELECT DISTINCT
+	LastName
+FROM
+	Person.Person
